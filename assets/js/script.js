5df7c3bd771d15d655cee27ad446fba09832806d
Danidu Viraj <daniduijse65@Gmail.com>
2023-11-10 19:14:04 +0530

    update portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 let open_menu = $("#open-menu");
 let close_menu = $("#close-menu");
 let menu_list = $("#menu-list");
+let sections = $("#menu-list > li > a");
 let menu_bg = $("#menu-bg");
 
 open_menu.on("click" , function () {
-    console.log("hello");
     open_menu.addClass("hide");
     close_menu.addClass("show");
 
@@ -14,12 +14,17 @@ open_menu.on("click" , function () {
 });
 
 close_menu.on("click" , function () {
-    console.log("hello");
     open_menu.removeClass("hide");
     close_menu.removeClass("show");
 
     menu_list.removeClass("show-flex");
     menu_bg.removeClass("show");
+
+});
+
+sections.on("click" , function () {
+    console.log("hello");
+    close_menu.click();
 
 });
 
